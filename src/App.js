5d7b45f38b56e9466ae6d797a92019ead82bd643
Daniel Destefanis <danieldestefanis@Daniels-MacBook-Pro.local>
2018-11-04 12:36:04 -0800

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Commuter
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
